49b8bcf40fdf48697ea9a0ca5bf0470df6d1e39b
Tomás Rama <tomas.rama@correo.ucu.edu.uy>
2021-09-05 16:13:58 -0300

    Comentario SRP

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -23,6 +23,14 @@ namespace SRP
             this.SectorBiblioteca = sector;
             this.EstanteBiblioteca = estante;
         }
+        /*
+        No se cumple con el principio SRP ya que esta clase tiene mas de un motivo para cambiar,
+        el método AlmacenarLibro puede cambiar dependiendo de las necesidades del usario, ej: Se podría agregar un edificio
+        como parámetro, entonces ya se estaría modificando la misma, o quizás se requiere almacenar el libro en forma virtual,
+        por lo tanto, existe más de un motivo para cambiar la clase.
+        Por otra parte es una responsabilidad que no le corresponde a la clase libro, saber donde está almacenado,
+        sino que a otra clase como puede ser "Almacenar", que le corresponde saber que libro es.
+        */
 
     }
 }
